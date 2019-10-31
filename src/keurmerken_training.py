270c345b4f3bbcee08f89833762d5614613b1a25
Jesse Haenen <jessehaenen@gmail.com>
2019-10-31 11:11:56 +0100

    update scripts with updated file structure

--- src/keurmerken_training.py
+++ src/keurmerken_training.py
@@ -1,9 +1,8 @@
 from imageai.Detection.Custom import DetectionModelTrainer
 
 # Choose path where data and model is stored
-DATA_DIR = r"C:\Users\judit\Downloads\drive-download-20191024T074253Z-001\planetproof"
-PRETRAINED_MODEL = r"C:\Users\judit\Downloads\drive-download-20191024T074253Z-001\planetproof\models\detection_model-ex-020--loss-0006.402.h5"
-
+DATA_DIR = "../data/"
+MODEL_PATH = "../models/planetproof_027_3288.h5"
 
 def keurmerk_train(data, model):
     """Trains YOLOv3 model from pretrained model.
@@ -19,7 +18,7 @@ def keurmerk_train(data, model):
     trainer = DetectionModelTrainer()
     trainer.setModelTypeAsYOLOv3()
 
-    labels = ['planetproof']
+    labels = ["planetproof"]
 
     # Data folder
     trainer.setDataDirectory(data_directory=DATA_DIR)
@@ -28,10 +27,10 @@ def keurmerk_train(data, model):
     trainer.setTrainConfig(object_names_array=labels,
                            batch_size=4,
                            num_experiments=100,
-                           train_from_pretrained_model=PRETRAINED_MODEL)
+                           train_from_pretrained_model=MODEL_PATH)
 
     trainer.trainModel()
 
 
 if __name__ == "__main__":
-    keurmerk_train(DATA_DIR, PRETRAINED_MODEL)
+    keurmerk_train(DATA_DIR, MODEL_PATH)
